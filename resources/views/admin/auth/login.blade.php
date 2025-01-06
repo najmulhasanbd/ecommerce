99b7ad1ffd89ca815724041b4c162794ec508769
najmul57 <najmulcse247@gmail.com>
2025-01-06 12:02:45 +0600

    admin login page setup

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
     <h4 class="text-center">Admin Login</h4>
@@ -44,4 +44,108 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Admin Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('backend') }}/assets/images/favicon.ico">
+
+    <!-- Vendor css -->
+    <link href="{{ asset('backend') }}/assets/css/vendor.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href="{{ asset('backend') }}/assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-style" />
+
+    <!-- Icons css -->
+    <link href="{{ asset('backend') }}/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Theme Config Js -->
+    <script src="{{ asset('backend') }}/assets/js/config.js"></script>
+</head>
+
+<body>
+    <div class="auth-bg d-flex min-vh-100 justify-content-center align-items-center">
+        <div class="row g-0 justify-content-center w-100 m-xxl-5 px-xxl-4 m-3">
+            <div class="col-xl-3 col-lg-4 col-md-6">
+                <div class="card overflow-hidden text-center rounded-4 p-xxl-4 p-3 mb-0">
+                    <a href="index.html" class="auth-brand mb-4">
+                        <img src="{{ asset('backend') }}/assets/images/logo-dark.png" alt="dark logo" height="26"
+                            class="logo-dark">
+                        <img src="{{ asset('backend') }}/assets/images/logo.png" alt="logo light" height="26"
+                            class="logo-light">
+                    </a>
+
+                    <h4 class="fw-semibold mb-2 fs-18">Admin Login</h4>
+
+                    <form method="POST" action="{{ route('admin.login') }}">
+                        @csrf
+                        <div class="mb-2">
+                            <input type="email" name="email" value="{{ old('email') }}" required autofocus
+                                autocomplete="email" class="form-control" placeholder="Enter your email">
+
+                            @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <input type="password" name="password" required autocomplete="current-password"
+                                class="form-control" placeholder="Enter your password">
+
+                            @error('password')
+                                <h6 class="text-danger">{{ $message }}</h6>
+                            @enderror
+                        </div>
+
+                        <div class="d-flex justify-content-between mb-3">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="checkbox-signin">
+                                <label class="form-check-label" for="checkbox-signin">{{ __('Remember me') }}</label>
+                            </div>
+
+                            @if (Route::has('password.request'))
+                                <a href="{{ route('password.request') }}"
+                                    class="text-muted border-bottom border-dashed">{{ __('Forgot your password?') }}</a>
+                            @endif
+                        </div>
+
+                        <div class="d-grid">
+                            <button class="btn btn-primary fw-semibold" type="submit"> {{ __('Log in') }}</button>
+                        </div>
+                    </form>
+
+
+                    {{-- <p class="text-muted fs-14 mb-0">Don't have an account?
+                        <a href="auth-register.html" class="fw-semibold text-danger ms-1">Sign Up !</a> --}}
+                    </p>
+                </div>
+
+                <p class="mt-4 text-center mb-0">
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script> © Ecommerce - By <span
+                        class="fw-bold text-decoration-underline text-uppercase text-reset fs-12">Md Najmul Hasan</span>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Vendor js -->
+    <script src="{{ asset('backend') }}/assets/js/vendor.min.js"></script>
+
+    <!-- App js -->
+    <script src="{{ asset('backend') }}/assets/js/app.js"></script>
+
+</body>
+
+</html>
