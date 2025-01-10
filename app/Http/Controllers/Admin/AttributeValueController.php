5f6656bd248ed790b105e920d6ab5d637746bddb
unknown <najmulcse247@gmail.com>
2025-01-10 10:11:14 +0600

    value add

--- app/Http/Controllers/Admin/AttributeValueController.php
+++ app/Http/Controllers/Admin/AttributeValueController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\AttributeValue;
 use Illuminate\Http\Request;
 
 class AttributeValueController extends Controller
 {
-    //
+    protected $attributeValue;
+    public function __construct(AttributeValue $attributeValue)
+    {
+        $this->attributeValue = $attributeValue;
+    }
+
+    public function index()
+    {
+        $data = $this->attributeValue::latest()->get();
+        return view('admin.attribute-value.index', compact('data'));
+    }
+    public function create() {}
+    public function store() {}
+    public function edit() {}
+    public function update() {}
+    public function destroy() {}
 }
--- resources/views/admin/attribute-value/index.blade.php
+++ resources/views/admin/attribute-value/index.blade.php
@@ -4,12 +4,12 @@
     <div class="page-content">
         <div class="page-title-head d-flex align-items-center ">
             <div class="flex-grow-1">
-                <h4 class="fs-18 fw-bold mb-0">Attribute</h4>
+                <h4 class="fs-18 fw-bold mb-0">Attribute Value</h4>
             </div>
 
             <div class="text-end">
                 <ol class="breadcrumb m-0 py-0 fs-13">
-                    <li class="breadcrumb-item"><a href="javascript: void(0);">Attribute</a></li>
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Attribute Value</a></li>
 
                     <li class="breadcrumb-item active">Dashboard</li>
                 </ol>
@@ -26,6 +26,7 @@
                                     <tr>
                                         <th>SL</th>
                                         <th>Name</th>
+                                        <th>Status</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
@@ -36,15 +37,20 @@
                                             <td>{{ $loop->iteration }}</td>
                                             <td>{{ ucwords($item->name) }}</td>
                                             <td>
-                                                <a href="{{route('attribute-value.index')}}"
-                                                    class="btn btn-sm btn-primary">
-                                                    Details
-                                                </a>
-                                                <a href="{{ route('attribute.edit', $item->id) }}"
+                                                @if ($item->status == 1)
+                                                    <a href="{{ route('attribute-value.inactive', $item->id) }}" id="inactive"
+                                                        class="btn btn-sm btn-success">Active</a>
+                                                @else
+                                                    <a href="{{ route('attribute-value.active', $item->id) }}" id="active"
+                                                        class="btn btn-sm btn-danger">Inactive</a>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <a href="{{ route('attribute-value.edit', $item->id) }}"
                                                     class="btn btn-sm btn-success">
                                                     <i class="ri-pencil-line"></i>
                                                 </a>
-                                                <a href="{{ route('attribute.delete', $item->id) }}" id="delete"
+                                                <a href="{{ route('attribute-value.delete', $item->id) }}" id="delete"
                                                     class="btn btn-sm btn-danger">
                                                     <i class="ri-delete-bin-line"></i>
                                                 </a>
