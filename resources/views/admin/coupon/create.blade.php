49533de01f0c367210fb31f538fda3b5723fd227
najmul57 <najmulcse247@gmail.com>
2025-01-13 13:46:09 +0600

    coupon page design

--- resources/views/admin/coupon/create.blade.php
+++ resources/views/admin/coupon/create.blade.php
@@ -13,29 +13,62 @@
         </div>
         <div class="page-container">
             <div class="row">
-                <div class="col-12 col-md-6 card p-3 mx-auto">
+                <div class="col-12 col-md-10 card p-3 mx-auto">
                     <form action="{{ route('coupon.store') }}" method="post" enctype="multipart/form-data">
                         @csrf
-                        <div class="form-group">
-                            <label for="name" style="width: 100%;text-align:start">Category Name</label>
-                            <input type="text" name="name" id="name" class="form-control"
-                                placeholder="enter category name" value="{{ old('name') }}">
-                            @error('name')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                        </div>
-                        <div class="form-group py-1">
-                            <label for="image" style="width: 100%; text-align: start;">Image</label>
-                            <input type="file" name="image" class="form-control" id="image"
-                                onchange="previewImage(event)">
-
-                            <!-- Display the preview image here -->
-                            <div id="imagePreviewContainer" style="margin-top: 10px;">
-                                <img id="imagePreview" src="#" alt="Image Preview"
-                                    style="max-width: 100px; display: none;" />
-                            </div> @error('image')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
+                        <div class="row">
+                            <div class="col-12 col-lg-6 mb-2">
+                                <div class="form-group">
+                                    <label for="code" style="width: 100%;text-align:start">Code </label>
+                                    <input type="text" name="code" id="code" class="form-control"
+                                        placeholder="enter code" value="{{ old('code') }}">
+                                    @error('code')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6 mb-2">
+                                <div class="form-group">
+                                    <label for="type" style="width: 100%;text-align:start">Discount Type</label>
+                                    <select name="type" id="type" class="form-select">
+                                        <option value="1">Fixed</option>
+                                        <option value="2">Percent</option>
+                                    </select>
+                                    @error('type')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6 mb-2">
+                                <div class="form-group">
+                                    <label for="amount" style="width: 100%;text-align:start">Discount Amount</label>
+                                    <input type="text" name="amount" id="amount" class="form-control"
+                                        placeholder="enter amount" value="{{ old('amount') }}">
+                                    @error('amount')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6 mb-2">
+                                <div class="form-group">
+                                    <label for="totalUseLimit" style="width: 100%;text-align:start">Total Use Limit</label>
+                                    <input type="text" name="totalUseLimit" id="totalUseLimit" class="form-control"
+                                        placeholder="Total Use Limit" value="{{ old('totalUseLimit') }}">
+                                    @error('totalUseLimit')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6 mb-2">
+                                <div class="form-group">
+                                    <label for="expireDate" style="width: 100%;text-align:start">Expire Date</label>
+                                    <input type="date" name="expireDate" id="expireDate" class="form-control"
+                                        value="{{ old('expireDate') }}">
+                                    @error('expireDate')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
                         </div>
                         <button type="submit" class="btn btn-success">Submit</button>
                     </form>
@@ -43,20 +76,4 @@
             </div>
         </div>
     </div>
-    <script>
-        function previewImage(event) {
-            const reader = new FileReader();
-            const file = event.target.files[0];
-
-            reader.onload = function() {
-                const imagePreview = document.getElementById('imagePreview');
-                imagePreview.src = reader.result;
-                imagePreview.style.display = 'block';
-            }
-
-            if (file) {
-                reader.readAsDataURL(file);
-            }
-        }
-    </script>
 @endsection
