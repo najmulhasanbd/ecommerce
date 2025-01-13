d267aa3311e821152f34aee765d854c49cf29402
najmul57 <najmulcse247@gmail.com>
2025-01-13 14:13:03 +0600

    coupon edit

--- resources/views/admin/coupon/edit.blade.php
+++ resources/views/admin/coupon/edit.blade.php
@@ -13,54 +13,60 @@
         </div>
         <div class="page-container">
             <div class="row">
-                <div class="col-12 col-md-6 card p-3 mx-auto">
-                    <form action="{{ route('category.update', $data->id) }}" method="POST" enctype="multipart/form-data">
+                <div class="col-12 col-md-10 card p-3 mx-auto">
+                    <form action="{{ route('coupon.update', $data->id) }}" method="post">
                         @csrf
                         @method('PUT')
-
-                        <div class="form-group">
-                            <label for="name" style="width: 100%; text-align:start">Category Name</label>
-                            <input type="text" name="name" id="name" class="form-control"
-                                value="{{ old('name', $data->name) }}">
-                            @error('name')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
+                        <div class="row">
+                            <div class="col-12 col-lg-6 mb-2">
+                                <div class="form-group">
+                                    <label for="code" style="width: 100%;text-align:start">Code </label>
+                                    <input type="text" name="code" id="code" class="form-control"
+                                        value="{{ old('code', $data->code) }}">
+                                    @error('code')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6 mb-2">
+                                <div class="form-group">
+                                    <label for="type" style="width: 100%;text-align:start">Discount Type</label>
+                                    <select name="type" id="type" class="form-select">
+                                        <option value="1" @if ($data->type == 1) selected @endif>Fixed
+                                        </option>
+                                        <option value="2" @if ($data->type == 2) selected @endif>Percent
+                                        </option>
+                                    </select>
+                                    @error('type')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6 mb-2">
+                                <div class="form-group">
+                                    <label for="amount" style="width: 100%;text-align:start">Discount Amount</label>
+                                    <input type="text" name="amount" id="amount" class="form-control"
+                                        value="{{ old('amount', $data->amount) }}">
+                                    @error('amount')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6 mb-2">
+                                <div class="form-group">
+                                    <label for="expireDate" style="width: 100%; text-align: start;">Expire Date</label>
+                                    <input type="date" name="expireDate" id="expireDate" class="form-control"
+                                    value="{{ old('expireDate', $data->expireDate ?? '') }}">                             
+                                    @error('expireDate')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
                         </div>
-
-                        <div class="form-group py-1">
-                            <label for="image" style="width: 100%; text-align:start">Image</label>
-                            <input type="file" name="image" class="form-control" id="image" onchange="previewImage(event)">
-                            
-                            <!-- Display the preview image here -->
-                            <img class="py-2" id="imagePreview" src="{{ asset('storage/categories/' . $data->image) }}" width="100px" height="100px" style="object-fit: cover" alt="Image Preview">
-                            
-                            @error('image')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                        </div>
-
                         <button type="submit" class="btn btn-success">Submit</button>
                     </form>
-
                 </div>
             </div>
         </div>
     </div>
-
-    <script>
-        function previewImage(event) {
-            const reader = new FileReader();  
-            const file = event.target.files[0];  
-    
-            reader.onload = function() {
-                const imagePreview = document.getElementById('imagePreview');
-                imagePreview.src = reader.result; 
-                imagePreview.style.display = 'block';  
-            }
-    
-            if (file) {
-                reader.readAsDataURL(file);
-            }
-        }
-    </script>
 @endsection
