09d6f48632e7cd708a676902f841a0f4de0eefc6
unknown <najmulcse247@gmail.com>
2025-02-19 23:14:16 +0600

    dfd

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -194,6 +194,7 @@
 <div id="preloader-active">
     <div class="preloader d-flex align-items-center justify-content-center">
         <div class="preloader-inner position-relative">
+            
             <div class="text-center">
                 <img src="{{asset('frontend')}}/assets/imgs/theme/loading.gif" alt="" />
             </div>
