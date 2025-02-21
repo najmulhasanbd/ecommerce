31312a5f4740aa74567f9f9dc110c7d2f7accf46
unknown <najmulcse247@gmail.com>
2025-02-21 12:40:56 +0600

    details page running

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -14,10 +14,24 @@ class FrontendController extends Controller
     {
         $data['banners'] = Banner::where('status', 1)->latest()->get();
         $data['categories'] = Category::where('status', 1)->latest()->get();
-        $data['products'] = Product::where('status', 1)->latest()->limit(10)->get();
+        $data['products'] = Product::with('category')->where('status', 1)->latest()->limit(10)->get();
         // $data['featured_products'] = Product::where('status', 1)->where('featured', 1)->latest()->limit(6)->get();
         // $data['hot_deals_products'] = Product::where('status', 1)->where('hot_deals', 1)->latest()->limit(6)->get();
 
         return view('frontend.index', $data);
+    }
+
+    public function ProductDetails($id)
+    {
+        $product = Product::findOrFail($id);
+
+        $color = $product->colors;
+        $product_color = explode(',', $color);
+
+        $size = $product->sizes;
+        $product_size = explode(',', $size);
+
+
+        return view('frontend.product.details', compact('product','product_color','product_size'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ require __DIR__.'/admin_auth.php';
 
 
 Route::get('/',[FrontendController::class,'index'])->name('frontend');
+
+Route::get('product/details/{id}',[FrontendController::class,'ProductDetails'])->name('product.details');
